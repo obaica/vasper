2fbb008e60b2b30ec066f833ebd7897de45de64c
kei0822kei <mizokami.keiyu@gmail.com>
2019-01-09 21:15:02 +0900

    job

--- auto/alm.zsh
+++ auto/alm.zsh
@@ -2,6 +2,12 @@
 
 ##### $1 : alm_auto.conf file
 
+### check
+if [ "$1" = "" ]; then
+  echo "$1 is not set"
+  exit 1
+fi
+
 ### source
 source $HOME/.vasperrc
 source $MODULE_DIR/qsystem.zsh
@@ -12,6 +18,7 @@ PHONOPY_DIR=$BASE_DIR/phonopy
 
 ### read alm_auto.conf
 MESH=`cat "$1" | sed s/"MESH = "/""/g`
+SLEEP_MINITES=`cat "$1" | sed s/"SLEEP_MINITES = "/""/g`
 MESH_NO_SPACE=`echo "$MESH" | sed s/" "/""/g`
 DOS_CONF=dos-m${MESH_NO_SPACE}.conf
 
@@ -27,26 +34,41 @@ function phonopy_working()
   cd $BASE_DIR
 }
 
+function check_finish()
+{
+  ALL_JOBS=`revised_qstat | cut -d " " -f 1`
+  for i in $JOB_IDS
+  do
+    if `echo $ALL_JOBS | grep -q $i`; then
+      return 1
+    fi
+    done
+    return 0
+}
 
 ### phonopy directory
-mkdir -p $PHONOPY_DIR
-vasper-makefile.zsh --get_conf "dos" "disp_alm.conf" "$MESH"
-mv $DOS_CONF $PHONOPY_DIR
-cp "BORN" "POSCAR-unitcell" "calc0/FORCE_SETS" $PHONOPY_DIR
-phonopy_working "0"
-
-
+if [ ! -e $PHONOPY_DIR ]; then
+  mkdir -p $PHONOPY_DIR
+  vasper-makefile.zsh --get_conf "dos" "disp_alm.conf" "$MESH"
+  mv $DOS_CONF $PHONOPY_DIR
+  cp "BORN" "POSCAR-unitcell" "calc0/FORCE_SETS" $PHONOPY_DIR
+  phonopy_working "0"
+fi
 
 ### alm calculation
 vasper-makedir.zsh --disp "disp_alm.conf"
 cd `ls | grep calc | sort | tail -n 1`
 vasper-qsub.zsh --disp "alm"
-# sleep 10
-# JOB_IDS=(`tail -n +2 "vasper_job.log" | cut -d " " -f 1`)
-# ALL_JOBS=`revised_qstat | cut -d " " -f 1`
-# 
-# 
-# 
+JOB_IDS=(`tail -n +2 "vasper_job.log" | cut -d " " -f 1`)
+
+while ! `check_finish`
+do
+  sleep $SLEEP_MINITES
+  date
+done
+
+echo "job finish !"
+
 # vasper-makefile.zsh --force_sets "alm"
 # cp FORCE_SETS $PHONOPY_DIR
 # cd $PHONOPY_DIR
---
+++ auto/hoge.zsh
@@ -0,0 +1,7 @@
+a=3
+
+
+while [ "$a" = "3" ]
+do
+  echo hoge
+done
