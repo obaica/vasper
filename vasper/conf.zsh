6bf2499ae3702eb136b52e3f57131f1420c1d666
kei0822kei <mizokami.keiyu@gmail.com>
2019-05-15 13:18:52 +0900

    revise reciprocal

--- vasper/conf.zsh
+++ vasper/conf.zsh
@@ -101,14 +101,29 @@ function revise_conf_param()
     return 251
   fi
   tmpfile=$(mktemp)
-  echo "revising : $PARAM_LINE => $2=\"$3\""
-  cat "$1" | sed s/"$PARAM_LINE"/"${2}=\"$3\""/g >> $tmpfile
+  cat "$1" | grep -v "$2" > $tmpfile
+  echo "${2}=\"$3\"" >> $tmpfile
   rm -f $1
   mv $tmpfile $1
-  echo ""
-  echo "~~ revised $1 ~~"
-  cat $1
 }
+
+# function revise_conf_param()
+# {
+#   ##### $1: conf file
+#   ##### $2: param name
+#   ##### $3: var
+#   PARAM_LINE=`cat "$1" | grep "${2}="`
+#   if [ "$PARAM_LINE" = "" ]; then
+#     echo "$2 does not exist in $1"
+#     echo "return(251)"
+#     return 251
+#   fi
+#   tmpfile=$(mktemp)
+#   echo "revising : $PARAM_LINE => $2=\"$3\""
+#   cat "$1" | sed s/"$PARAM_LINE"/"${2}=\"$3\""/g >> $tmpfile
+#   rm -f $1
+#   mv $tmpfile $1
+# }
 
 function make_new_conf_line_when_not_found()
 {
@@ -120,8 +135,5 @@ function make_new_conf_line_when_not_found()
     echo "there is no $2 param in $1 file, so make new line"
     echo "additional line is below"
     echo "$2=\"$3\"" | tee -a $1
-    echo ""
-    echo "~~ revised $1 ~~"
-    cat $1
   fi
 }
